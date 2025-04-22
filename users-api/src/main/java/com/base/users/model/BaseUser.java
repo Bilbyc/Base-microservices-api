aacc07af82561625dc0b5622726ce31b38bf129b
Carlos Bilby <carlos.bilby@gmail.com>
2025-04-22 18:37:48 -0300

    refactor: - renomeia User para BaseUser para evitar conflitos de import - gera getters, setters e construtor

---
+++ users-api/src/main/java/com/base/users/model/BaseUser.java
@@ -0,0 +1,46 @@
+package com.base.users.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class BaseUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+//    private String role;
+
+
+    public BaseUser() {
+    }
+
+    public BaseUser(Long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
