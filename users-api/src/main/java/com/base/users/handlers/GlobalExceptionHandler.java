e9a89986fc9cb271732161d9dc93041c93a77921
Carlos Bilby <carlos.bilby@gmail.com>
2025-04-23 01:13:37 -0300

    feat: adiciona o exception handler global para validar campos de endpoints

---
+++ users-api/src/main/java/com/base/users/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.base.users.handlers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
+
