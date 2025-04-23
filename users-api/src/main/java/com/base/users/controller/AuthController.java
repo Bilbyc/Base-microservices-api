acc9a98acb1ac20de57a4862fbdf9012e285e1ca
Carlos Bilby <carlos.bilby@gmail.com>
2025-04-22 22:36:14 -0300

    refactor: altera caminho de endpoints de autenticação

--- users-api/src/main/java/com/base/users/controller/AuthController.java
+++ users-api/src/main/java/com/base/users/controller/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collections;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
     private AuthenticationConfiguration authenticationConfiguration;
     private UserService userService;
